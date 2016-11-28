2fce848af729db25cbf46e4d95f4a13c5a4b1ba6
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2016-11-28 22:09:19 +0100

    Disallow users to vote more then once

--- web/app/themes/moppen/assets/scripts/rating.js
+++ web/app/themes/moppen/assets/scripts/rating.js
@@ -1,7 +1,13 @@
+import '../../bower_components/js-cookie/src/js.cookie';
+
 jQuery(($) => {
   let upvotes = parseInt($('.rating-bar').data('upvotes'), 10);
   let downvotes = parseInt($('.rating-bar').data('downvotes'), 10);
   let rating = parseFloat($('.rating-bar').data('rating'), 10);
+
+  const postId = window.ajax_object.post_id;
+  const voted = [];
+  let votedisabled = false;
 
   function updateRating() {
     /**
@@ -18,7 +24,7 @@ jQuery(($) => {
       url: window.ajax_object.ajax_url,
       data: {
         action: 'update_rating',
-        post_id: window.ajax_object.post_id,
+        post_id: postId,
         upvotes,
         downvotes,
         rating,
@@ -31,17 +37,32 @@ jQuery(($) => {
     const width = rating * 100;
     $('.rating').addClass('voted');
     $('.rating .bar').css('width', width + '%');
-    $('.rating .grade').html(grade);
+    $('.rating .grade').html(grade.toFixed(1));
     $('.rating .votes').html(totalvotes);
+
+    /**
+     * Set cookie
+     */
+    voted.push(postId);
+    Cookies.set('voted', voted);
+
+    /**
+     * Disable voting
+     */
+    votedisabled = true;
   }
 
   $('.downvote .button').click(() => {
-    downvotes++;
-    updateRating();
+    if (!votedisabled) {
+      downvotes++;
+      updateRating();
+    }
   });
 
   $('.upvote .button').click(() => {
-    upvotes++;
-    updateRating();
+    if (!votedisabled) {
+      upvotes++;
+      updateRating();
+    }
   });
 });
