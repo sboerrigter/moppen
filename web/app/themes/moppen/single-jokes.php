05cc717028ad03faa4da68ebf92215d9968ec7be
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2016-11-28 21:42:57 +0100

    Improved pagination and next post link

--- web/app/themes/moppen/single-jokes.php
+++ web/app/themes/moppen/single-jokes.php
@@ -3,6 +3,12 @@ $context = Timber::get_context();
 $context['post'] = new Sboerrigter\Moppen\TimberHelpers\Joke();
 
 $context['archivelink'] = get_post_type_archive_link('jokes');
-$context['nextpostlink'] = get_permalink(get_adjacent_post(false, '', true));
+
+$randomjoke = get_posts(array(
+    'orderby' =>        'rand',
+    'posts_per_page' => '1',
+    'post_type' =>      'jokes'
+));
+$context['nextpostlink'] = get_permalink($randomjoke[0]->ID);
 
 Timber::render('single-jokes.twig', $context);
