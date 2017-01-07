c16c3d5e82424e901ffcecd2730f5e24e7229fb9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-01-07 12:13:37 +0100

    :wrench: Capistrano

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'capistrano', '~> 3.2.0'
+gem 'capistrano', '~> 3.6'
 gem 'capistrano-composer'
 gem 'capistrano-wpcli'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # WP-CLI
-set :wpcli_local_url, 'http://moppen.dev'
+set :wpcli_local_url, 'http://moppen.local'
 set :wpcli_remote_url, 'http://moppen.site'
 
 # Server settings
-server 'moppen.site', user: 'serverpilot', roles: %w{web app db}
+server '128.199.43.35', user: 'serverpilot', roles: %w{web app db}
