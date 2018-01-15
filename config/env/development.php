db2ebc656b354e4050b5f1c3a8e2fcc6dc9f86cc
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-01-15 23:16:02 +0100

    :wrench: Error reporting

--- config/env/development.php
+++ config/env/development.php
@@ -2,15 +2,15 @@
 /**
  * Error reporting
  */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_DISPLAY', true );
-define( 'WP_DEBUG_LOG', true );
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
 
-define( 'SAVEQUERIES', true );
+define('SAVEQUERIES', true);
 
 /**
  * E-mails
  *
  * @see https://github.com/trendwerk/dev-mail
  */
-define( 'TP_DEV_MAIL', 'info@trendwerk.nl' );
+define('TP_DEV_MAIL', 'info@trendwerk.nl');
