60a5c365dece50ec727b61f105b326072e2bd901
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2024-07-03 21:20:22 +0200

    :construction_worker: New server config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,30 @@
 set :application, 'moppen'
 set :repo_url, 'git@github.com:sboerrigter/moppen.git'
-
-# Branch
 set :branch, :master
 
-# Deployment folder
-set :deploy_to, -> { "/srv/users/serverpilot/apps/#{fetch(:application)}" }
-
-# Log level
 set :log_level, :info
 
-# Linked files and directories for /shared/
-set :linked_files, fetch(:linked_files, []).push('.env', 'auth.json')
-set :linked_files, fetch(:linked_files, []).push('web/.htaccess')
-set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
+set :linked_files, ['.env', 'auth.json', 'web/.htaccess']
+set :linked_dirs, ['web/app/uploads']
+
+set :wpcli_local_url, 'http://moppen.test'
+
+after 'deploy:published', 'opcache:clear'
+after 'deploy:published', 'varnish:clear'
+
+namespace :opcache do
+  task :clear do
+    on roles(:app) do
+      execute :'php-fpm-cli', "-r 'opcache_reset();'"
+    end
+  end
+end
+
+namespace :varnish do
+  task :clear do
+    on roles(:app) do
+      execute :'curl', "-sX BAN -H 'X-Ban-Method: exact' -H 'X-Ban-Host: ", fetch(:varnish_url), "' http://localhost/ > /dev/null"
+    end
+  end
+end
+
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-# WP-CLI
-set :wpcli_local_url, 'http://moppen.test'
-set :wpcli_remote_url, 'http://moppen.site'
+# ssh moppen.site@tw-server-05.twservices.eu -p2223
+server 'tw-server-05.twservices.eu', user: 'moppen.site', roles: %w{web app db}, port: 2223
 
-# Server settings
-server '128.199.43.35', user: 'serverpilot', roles: %w{web app db}
+set :deploy_to, -> { '/var/www/vhosts/TB01-004/moppen.site/site' }
+set :varnish_url, 'moppen.site'
+set :wpcli_remote_url, 'https://moppen.site'
